965a4e57a494132b5d80d63829c99bed95a1fa9d
Philip Lombardi <plombardi@datawire.io>
2017-04-22 17:33:06 -0400

    Overwrite any existing packer in /usr/local/bin

--- ci/travis_before_script.sh
+++ ci/travis_before_script.sh
@@ -5,7 +5,7 @@ pip install -Ur ci/requirements.txt
 curl --output packer_${PACKER_VERSION}_linux_amd64.zip \
      https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
 
-unzip packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin
+unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin
 rm -f packer_${PACKER_VERSION}_linux_amd64.zip
 
 packer version
