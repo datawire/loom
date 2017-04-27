12125e3c45841a9004d13c8487cdaf1975e75fe7
Philip Lombardi <plombardi@datawire.io>
2017-04-27 16:25:35 -0400

    Turn echo off for docker login

--- ci/travis_script.sh
+++ ci/travis_script.sh
@@ -3,13 +3,8 @@ set -euxo pipefail
 
 ./gradlew clean test shadowJar
 
+set +x
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-ci/dockerize.py --push
+set -x
 
-#if [ "${TRAVIS_BRANCH}" = "master" ]; then
-#    packer build \
-#        -var "build_number=${TRAVIS_BUILD_NUMBER}" \
-#        -var "branch=${TRAVIS_BRANCH}" \
-#        -var "commit=${TRAVIS_COMMIT}" \
-#        src/ami/centos-7.json
-#fi
+ci/dockerize.py --push
