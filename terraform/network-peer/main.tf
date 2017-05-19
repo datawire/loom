206317b928429a2708a1a7b34644e93f40e26185
Philip Lombardi <plombardi@datawire.io>
2017-05-19 13:00:30 -0400

    Update Terraform networking configuration
    - Peering arrangement for Kubernetes <-> External VPC
    - Variables for External VPC

---
+++ terraform/network-peer/main.tf
@@ -0,0 +1,69 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Variables
+// ---------------------------------------------------------------------------------------------------------------------
+
+variable "external_services_vpc" {
+  description = "VPC that contains the external (non-kubernetes) services."
+}
+
+variable "external_services_vpc_cidr" {
+  description = "VPC CIDR block for the external (non-kubernetes) services."
+}
+
+variable "external_services_vpc_external_route_table" {
+  description = "route table identifier for the external services external (public) route table"
+}
+
+variable "external_services_vpc_internal_route_tables" {
+  type        = "list"
+  description = "route table identifiers for the external services external (public) route table"
+}
+
+variable "kubernetes_vpc" {
+  description = "VPC that contains the Kubernetes cluster nodes."
+}
+
+variable "kubernetes_vpc_cidr" {
+  description = "VPC CIDR block for the external (non-kubernetes) services."
+}
+
+variable "kubernetes_vpc_route_table" {
+  description = "route table identifier for the "
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Resource Management
+// ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_vpc_peering_connection" "main" {
+  vpc_id      = "${var.external_services_vpc}" // "Requestor"
+  peer_vpc_id = "${var.kubernetes_vpc}"        // "Acceptor"
+  auto_accept = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
+
+resource "aws_route" "kubernetes-external_services" {
+  route_table_id            = "${data.aws_route_table.${kubernetes_vpc_route_table}.id}"
+  destination_cidr_block    = "${var.external_services_vpc_cidr}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.main.id}"
+}
+
+resource "aws_route" "external_services_external-kubernetes" {
+  route_table_id            = "${var.external_services_vpc_external_route_table}"
+  destination_cidr_block    = "${var.kubernetes_vpc}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.main.id}"
+}
+
+resource "aws_route" "external_services_internal-kubernetes" {
+  count                     = "${length(external_services_vpc_internal_route_tables)}"
+  route_table_id            = "${element(var.external_services_vpc_internal_route_tables, count.index)}"
+  destination_cidr_block    = "${var.kubernetes_vpc}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.main.id}"
+}
